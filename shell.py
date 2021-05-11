15a9693d3e77974b4aed991f9096bdda2e7d959e
justaprudev <justaprudev@gmail.com>
2021-05-11 23:52:26 +0530

    shell: updated to latest version

--- shell.py
+++ shell.py
@@ -1,23 +1,16 @@
 # This file is distributed as a part of the polygon project (justaprudev.github.io/polygon)
 # By justaprudev
 
-import io
-
-
 @polygon.on(prefix="$")
 async def shell(e):
     await e.edit("`Evaluating this command..`")
     cmd = e.text[1:]
     reply = await e.get_reply_message()
-    stdout, stderr = polygon.shell(cmd)
-    output = (
-        f"**Query:** \
+    output = f"**Query:** \
             \n`{cmd}` \
-            \n\n**stderr:** \
-            \n`{stderr}` \
-            \n\n**stdout:** \
-            \n `{stdout}`"
-    )
+            \n\n**Output:** \
+            \n`{polygon.shell(cmd)}`"
+
     if len(output) > db.get("TelegramLimit"):
         await polygon.send_file(
             e.chat_id,
